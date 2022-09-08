b2805dd84f7a94a61da196f2442476209e219a12
StefanMunyithya <stefanmunyithya@gmail.com>
2022-09-08 12:14:13 -0700

    Edited c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,14 +6,13 @@
 
 #include<stdio.h>
 
-/*
- * main - Starting point of program
+/**
+ * main - Entry point
  *
- * Returns 0 if executed well
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
