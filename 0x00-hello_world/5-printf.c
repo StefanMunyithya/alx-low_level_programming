356e47e58e0acccf91acf5269e861e6e76ea2a16
StefanMunyithya <stefanmunyithya@gmail.com>
2022-09-08 12:20:21 -0700

    Created c program that prints using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ *This program prints a string using the printf function
+ */
+
+#include<stdio.h>
+
+/*
+ *main - Entry point
+ *
+ * Returns (0) if ok
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
