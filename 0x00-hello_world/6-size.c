733eccc67cc4a946cc092ee527aaf4f52b751cb0
StefanMunyithya <stefanmunyithya@gmail.com>
2022-09-08 12:43:36 -0700

    Edited c program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 /**
- * This program prints sizes of types used in c
+ * This program prints sizes in bytes of types used in c
  */
 #include<stdio.h>
 
@@ -8,13 +8,12 @@
  *
  * Returns: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("Size of a char: %ld byte(s)\n",sizeof(char));
-	printf("Size of an int: %ld byte(s)\n",sizeof(int));
-	printf("Size of a long int: %ld byte(s)\n",sizeof(long int));
-	printf("Size of a long long int: %ld byte(s)\n",sizeof(long long int));
-	printf("Size of a float: %ld byte(s)\n",sizeof(float));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
