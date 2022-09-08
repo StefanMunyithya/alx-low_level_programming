cf0f8f27f74172e2b582fe94817583be3e5e023a
StefanMunyithya <stefanmunyithya@gmail.com>
2022-09-08 12:05:35 -0700

    Created first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/*
+ * Outputs string using puts function
+ *
+ * returns 0 if ok
+ */
+
+#include<stdio.h>
+
+int main()
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	
+	return 0;
+}
