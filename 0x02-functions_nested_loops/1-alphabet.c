17b758d19b11e9ece76ce2519a899594f5454584
StefanMunyithya <stefanmunyithya@gmail.com>
2022-09-13 19:37:50 +0300

    Created function to print the alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints letters in lower case
+ *
+ * Return: Nothing
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		++c;
+	}
+	
+	_putchar('\n');
+	return;
+}
