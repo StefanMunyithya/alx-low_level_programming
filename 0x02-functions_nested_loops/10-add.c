c4c4c0e405638b4c23d3b6ea9a1d0acfcc5bc480
StefanMunyithya <stefanmunyithya@gmail.com>
2022-09-14 22:47:48 +0300

    Created sum program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @n1: One of the integers in the sum
+ * @n2: The other integer in the sum
+ *
+ * Return: Sum
+ */
+int add(int n1, int n2)
+{
+	return(n1 + n2);
+
+}
