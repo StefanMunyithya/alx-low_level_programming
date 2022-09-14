f5ab59037005e3492a124c262ac1c5e7ff3fb451
StefanMunyithya <stefanmunyithya@gmail.com>
2022-09-14 21:12:58 +0300

    Absolute value program created

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computes absolute value of an integer
+ * @n: Integer which absolute values is to be determined
+ *
+ * Return: absolute value of integer
+ */
+int _abs(int n)
+{
+	if ( n < 1)
+		return (n * -1);
+	else
+		return (n);
+}
