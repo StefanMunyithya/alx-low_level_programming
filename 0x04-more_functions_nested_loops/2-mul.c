d2dbf691f7525aaad30c84f0e5e1ef26b31824e2
StefanMunyithya <stefanmunyithya@gmail.com>
2022-09-15 23:18:00 +0300

    Created multiply program

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Returns multiple of 2 integers
+ * @a: One of the integers of the expression
+ * @b: The other integer in the expression
+ *
+ * Return: Product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
