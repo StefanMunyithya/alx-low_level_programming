4d29797254d92ce00a1cc5fbffc9d039fb4e1b00
StefanMunyithya <stefanmunyithya@gmail.com>
2022-09-19 21:32:44 +0300

    Created print reverse program

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: Points to first character
+ *
+ * Return: Void
+ */
+void print_rev(char *s)
+{
+	char c;
+	int count;
+	int i;
+
+	count = 0;
+	c = *s;
+	while (c != '\0')
+	{
+		count++;
+		++s;
+		c = *s;
+	}
+
+	--s;
+	c = *s;
+
+	for (i = count; i > 0; --i)
+	{
+		_putchar(c);
+		--s;
+		c = *s;
+	}
+
+}
