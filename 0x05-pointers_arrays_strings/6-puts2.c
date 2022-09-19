184c9d0b10e4b9a6e2ca020573686442821796ec
StefanMunyithya <stefanmunyithya@gmail.com>
2022-09-19 22:16:54 +0300

    Created puts2 program

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other chacter of a string
+ * @str: First character
+ *
+ * Return: Void
+ */
+void puts2(char *str)
+{
+	char c;
+
+	c = *str;
+	while (c != '\0')
+	{
+		_putchar(c);
+		str += 2;
+		c = *str;
+	}
+}
