686da42d773bbbacc59ee3a89385f40b25b7c91e
StefanMunyithya <stefanmunyithya@gmail.com>
2022-10-04 07:19:23 +0300

    Edited create array program

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,6 +20,11 @@ char *create_array(unsigned int size, char c)
 	}
 	p = (char *)malloc(sizeof(char) * size);
 
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		*(p + i) = c;
