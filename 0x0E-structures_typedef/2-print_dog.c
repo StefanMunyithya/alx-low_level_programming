818fc09387a0268485644966837c646e27f09a82
StefanMunyithya <stefanmunyithya@gmail.com>
2022-10-12 21:36:10 +0300

    Created print dog function

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_dog - Prints dog record
+ * @d: Pointer to struct dog
+ * Return: Void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		exit(1);
+	}
+	printf("Name: ");
+	if ((*d).name == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", (*d).name);
+	printf("Age: ");
+	if ((*d).age == 0.0)
+		printf("(nil)\n");
+	else
+		printf("%f\n", (*d).age);
+	printf("Onwer: ");
+	if ((*d).owner == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", (*d).owner);
+}
