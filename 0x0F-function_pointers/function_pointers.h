24748914f1cd94502fdca246df756bd0db1baaec
StefanMunyithya <stefanmunyithya@gmail.com>
2022-10-14 05:11:05 +0300

    Edited function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
 #ifndef FUNCTION_POINTERS.H
 #define FUNCTION_POINTERS.H
 int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
 
-#endif /* FUNCTION_POINTERS>H */
+
+#endif /* FUNCTION_POINTERS.H */
