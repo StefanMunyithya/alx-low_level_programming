cb6905b8ebcf608f27ea202ce4ca5b0140c71f26
StefanMunyithya <stefanmunyithya@gmail.com>
2022-10-14 05:50:14 +0300

    Created int_index function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - First element of which cmp does
+ * not return 0
+ * @array: Array of integers
+ * @size: Size of the array
+ * @cmp: Comparison function
+ *
+ * Return: 1 is success
+ * -1 if fails
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+
+	}
+	return (-1);
+}
