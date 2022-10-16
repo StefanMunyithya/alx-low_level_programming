4d69a9ccfc3bd3a13487f473b48acb470b39d35d
StefanMunyithya <stefanmunyithya@gmail.com>
2022-10-16 19:18:50 +0300

    Edited new_dog function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,8 +19,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 	d->name = malloc(strlen(name) + 1);
+	if (d->name == NULL)
+		return (NULL);
 	strcpy(d->name, name);
 	d->owner = malloc(strlen(owner) + 1);
+	if (d->owner == NULL)
+		return (NULL);
 	strcpy(d->owner, owner);
 	d->age = age;
 
