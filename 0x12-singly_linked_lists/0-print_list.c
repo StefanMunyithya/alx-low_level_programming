059113849d7cdc9723d757af94fd3d851fbf6997
StefanMunyithya <stefanmunyithya@gmail.com>
2022-10-24 21:01:10 +0300

    Created printlist function

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints data in linked list
+ * @h: Pointer to head
+ *
+ * Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num_nodes;
+
+	if (h == NULL)
+	{
+		printf("[0] (nil)\n");
+		return (0);
+	}
+	num_nodes = 0;
+
+	while (h->next != NULL)
+	{
+		num_nodes++;
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	num_nodes++;
+	printf("[%u] %s\n", h->len, h->str);
+
+	return (num_nodes);
+
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory contains programs demonstrating use of singly linked lists
