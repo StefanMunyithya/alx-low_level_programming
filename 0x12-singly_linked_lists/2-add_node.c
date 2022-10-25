6e8c27fed9e7ee659a0ebb90057710067f3bee06
StefanMunyithya <stefanmunyithya@gmail.com>
2022-10-25 22:21:32 +0300

    Edited add_node function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,6 +11,9 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *ptr;
 
 	ptr = malloc(sizeof(list_t));
+
+	if (ptr == NULL)
+		return (NULL);
 	ptr->str = strdup(str);
 	ptr->len = strlen(str);
 
