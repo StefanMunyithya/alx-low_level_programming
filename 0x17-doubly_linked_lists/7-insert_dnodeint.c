8b9f64cc44acf658f66f037fa56d0c34c2cc0335
StefanMunyithya <stefanmunyithya@gmail.com>
2023-01-15 14:38:12 +0300

    Created insert node function

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Inserts node at given index
+ * @h: Double pointer to header node
+ * @idx: Index
+ * @n: Data
+ *
+ * Return: Pointer to added node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new;
+	dlistint_t *prev;
+	dlistint_t *ptr;
+	unsigned int current_index;
+
+	if (h == NULL)
+		return (NULL);
+	ptr = *h;
+	if (ptr == NULL)
+	{
+		if (idx == 0)
+		{
+			new = add_dnodeint(&ptr, n);
+			return (new);
+		}
+		else
+			return (NULL);
+	}
+	current_index = 0;
+	while (ptr->next != NULL)
+	{
+		if (current_index == idx)
+		{
+			if (idx == 0)
+			{
+				new = add_dnodeint(&ptr, n);
+				return (new);
+			}
+
+			prev = ptr->prev;
+			new = add_dnodeint(&ptr, n);
+			new->prev = prev;
+			prev->next = new;
+			return (new);
+		}
+		ptr = ptr->next;
+		current_index++;
+	}
+	if (current_index == idx)
+	{
+		new = add_dnodeint_end(h, n);
+		return (new);
+	}
+	return (NULL);
+}
